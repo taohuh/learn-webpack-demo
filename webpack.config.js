15465fbdcc4328766006912036c91da6d0f6e8f0
taohuh <pimpakarn@maqe.com>
2020-03-10 13:01:59 +0700

    Add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
+require("dotenv").config();
+
+const path = require("path");
+const webpack = require("webpack");
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
@@ -5,5 +10,22 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Webpack Demo",
     }),
+    new webpack.WatchIgnorePlugin([
+      path.join(__dirname, "node_modules"),
+    ]),
   ],
+  devServer: {
+    stats: "errors-only",
+    host: process.env.HOST,
+    port: process.env.PORT,
+    open: true,
+    overlay: true,
+    watchOptions: {
+      // Delay the rebuild after the first change
+      aggregateTimeout: 300,
+
+      // Poll using interval (in ms, accepts boolean too)
+      poll: 1000,
+    }
+  }
 };
